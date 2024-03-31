d8b511caffa34da9d4ec14563333c24a12ab1e59
karan <karanjaink8899@gmail.com>
2024-03-31 20:08:29 +0530

    resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import resume from "../assets/Karan-jain-Resume.pdf";
+import resume from "../assets/Karan jain Resume.pdf";
 
 function SocialLinks() {
   const sociallinks = [
